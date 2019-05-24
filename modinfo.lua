1fb1dbae406614602af905f354ee278342dac2f5
Artem Romanko <rinart73@gmail.com>
2019-05-24 03:08:42 +0200

    0.1

--- modinfo.lua
+++ modinfo.lua
@@ -4,7 +4,7 @@ meta =
     -- ID of your mod; Make sure this is unique!
     -- Will be used for identifying the mod in dependency lists
     -- Will be changed to workshop ID (ensuring uniqueness) when you upload the mod to the workshop
-    id = "Buffs",
+    id = "1749930520",
 
     -- Name of your mod; You may want this to be unique, but it's not absolutely necessary.
     -- This is an additional helper attribute for you to easily identify your mod in the Mods() list
@@ -40,7 +40,7 @@ meta =
     -- },
     dependencies = {
         {id = "1722652757", min = "1.2"},
-        {id = "Avorion", min = "0.23", max = "0.23"}
+        {id = "Avorion", exact = "0.23"}
     },
 
     -- Set to true if the mod only has to run on the server. Clients will get notified that the mod is running on the server, but they won't download it to themselves
